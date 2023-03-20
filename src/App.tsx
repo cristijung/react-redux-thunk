98079455d426e6611234d1f7ff863a4bf6cbc1bb
Cristina Jung <cristijung@gmail.com>
2023-03-20 17:08:12 -0300

    ajuste da estrutura

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,19 @@
 import React, {useState} from 'react';
-import { getComments } from './redux/actionCreators/getComment';
-import { useDispatch } from 'react-redux';
-import { useTypedSelector } from './hooks/useTypeSelector';
+// import { getComments } from './redux/actionCreators/getComment';
+// import { useDispatch } from 'react-redux';
+// import { useTypedSelector } from './hooks/useTypeSelector';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { Principal } from './components/Principal';
 
 function App() {
-  const dispatch = useDispatch();
-  const [postId, setPostID] = useState("");
-  const { comments, loading, error } = useTypedSelector((state) => state.comments);
-
-  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    await dispatch(getComments(postId));
-  }
-
+  
   return (
-      <div>
+      <>
         <Header />
-      <div>
-        <form onSubmit={onSubmitHandler}>
-          <input type={"number"} value={postId} onChange={(e) => setPostID(e.target.value)} /> 
-          <button type="submit" className='btn'> Enviar </button>
-          
-        </form>
-      </div>
-
-      {
-        loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul>
-            {
-              comments.map((comment) => {
-                return(<li key={comment.id}>{comment.body}</li>)
-              })
-            }
-          </ul>
-        )
-      }
-      <Footer />
-      </div>
+        <Principal />    
+        <Footer />
+      </>
   );
 }
 
--- src/components/Principal/index.tsx
+++ src/components/Principal/index.tsx
@@ -1,10 +1,43 @@
 import './Principal.css'
+import React, {useState} from 'react';
+import { getComments } from '../../redux/actionCreators/getComment';
+import { useDispatch } from 'react-redux';
+import { useTypedSelector } from '../../hooks/useTypeSelector'; 
 
 export function Principal(){
-    return(
+    const dispatch = useDispatch();
+  const [postId, setPostID] = useState("");
+  const { comments, loading, error } = useTypedSelector((state) => state.comments);
+
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await dispatch(getComments(postId));
+  }
+
+  return(
         <article className='main'>
-            Algo por agora
-            
+           <> 
+            <form onSubmit={onSubmitHandler}>
+          <input type={"number"} value={postId} onChange={(e) => setPostID(e.target.value)} /> 
+          <button type="submit" className='btn'> Enviar </button>
+          
+        </form>
+      
+
+      {
+        loading ? (
+          <div>Loading...</div>
+        ) : (
+          <ul>
+            {
+              comments.map((comment) => {
+                return(<li key={comment.id}>{comment.body}</li>)
+              })
+            }
+          </ul>
+        )
+      }
+           </>
         </article>
     );
 
